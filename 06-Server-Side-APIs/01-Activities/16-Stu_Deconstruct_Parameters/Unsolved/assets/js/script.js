502ab9989d5d2c287512edead296f26f6f71250d
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-01-27 18:21:12 -0600

    Updates

--- 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/16-Stu_Deconstruct_Parameters/Unsolved/assets/js/script.js
@@ -2,6 +2,10 @@ fetch(
   // Explain each parameter in comments below.
   'https://api.github.com/repos/nodejs/node/issues?per_page=10&state=open&sort=created&direction=desc'
 )
+
+//issues - 10 per Page
+//state - currently opened
+//sort - what to sort results by. (can either be created, updated, and comments)
   .then(function (response) {
     return response.json();
   })
@@ -9,3 +13,8 @@ fetch(
     console.log(data);
   });
 // Parameter explanation.
+
+
+
+// https://www.w3schools.com/test/names.asp?name=John&age=30
+// after the ? is the query string value => name=John&age=30 is the query string if you don't specify the query string.
