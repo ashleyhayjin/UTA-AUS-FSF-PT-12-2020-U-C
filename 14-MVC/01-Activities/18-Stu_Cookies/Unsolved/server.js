3608b5862106fa807bcba5d4205ac19bb350a7e2
Ashley Lee <ashleyhayjin@iMac.fios-router.home>
2021-04-02 19:06:16 -0500

    updates

--- 14-MVC/01-Activities/18-Stu_Cookies/Unsolved/server.js
+++ 14-MVC/01-Activities/18-Stu_Cookies/Unsolved/server.js
@@ -13,13 +13,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Add comments describing the implementation of sessions
+// creates a new session. 
+
 const sess = {
   secret: 'Super secret secret',
   // TODO: Add comments describing the implementation of cookies
+  // nothing is inside the cookies until you start adding things
+  // nothing will resafe?
+  //  
   cookie: {},
   resave: false,
   saveUninitialized: true,
   // TODO: Add comments describing the implementation of the store
+  // Stores it into a db called sequelize
   store: new SequelizeStore({
     db: sequelize,
   }),
